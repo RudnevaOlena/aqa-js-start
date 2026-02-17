48dbb8d90708835a7f0d9dacc7315b7bfb5db05b
Olena <rudneva7elena@gmail.com>
2026-02-17 01:19:36 +0100

    Add homework 4.1 primitive data types

---
+++ jsBasics/task4.js
@@ -0,0 +1,4 @@
+let radius = 12.5;
+let area = Math.PI * radius ** 2;
+console.log(`Area of circle with radius ${radius} is: ${area.toFixed(2)}`);
+
---
+++ jsBasics/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let radiusCylinder = 7.11;
+let heightCylinder = 11.11;
+let volumeCylinder = Math.PI * radiusCylinder ** 2 * heightCylinder;
+console.log(`Volume of cylinder with radius ${radiusCylinder} and height ${heightCylinder} is: ${volumeCylinder.toFixed(2)}`);
