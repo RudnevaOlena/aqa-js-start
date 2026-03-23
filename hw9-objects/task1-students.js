c4bb5c52513dc878bbff2e310fa50a0dd9622510
Olena <rudneva7elena@gmail.com>
2026-03-23 01:52:58 +0100

    test pre-commit hook after fixing syntax

--- hw9-objects/task1-students.js
+++ hw9-objects/task1-students.js
@@ -1,6 +1,6 @@
 const student = {};
-student.firstName = "Nick";
-student.lastName = "Smith";
+student.firstName = 'Nick';
+student.lastName = 'Smith';
 student.age = 20;
-student.courses = ["Math", "History", "Programming"];
+student.courses = ['Math', 'History', 'Programming'];
 console.log(student);
